4c4bb16f14403f40c40bad886cb0d95c20128322
Milad Parsa <82820198+miladparsa@users.noreply.github.com>
2023-05-11 22:48:15 +0330

    string interpolation

---
+++ string interpolation.py
@@ -0,0 +1,28 @@
+# string interpolation => formatting strings
+
+
+
+userName = "Milad"
+
+
+userFamily = "Parsa"
+
+number_1 = 3
+number_2 = 5
+
+result = "user name is " + userName + " and user family is " + userFamily
+
+# print(result)
+
+result2 = f"user name is {userName} and user family is {userFamily}"
+result3 = "user name is {userName} and user family is {userFamily}"
+# print(result3)
+
+# print(f"sum is : {number_1 + number_2}")
+
+
+# string indexes
+
+
+
+print(userName[4])
