da067590cb66902b07826d7953a0557147f6170b
Milad Parsa <82820198+miladparsa@users.noreply.github.com>
2023-05-11 22:45:43 +0330

    Name.py

---
+++ Name.py
@@ -0,0 +1,43 @@
+myName = "Milad"
+myFamily = 'Parsa'
+myAge = 27
+
+print(type(myName))
+print(type(myFamily))
+
+print('---------------')
+print(myName)
+print(myFamily)
+
+print('---------------')
+
+# milad said : " i am programmer "
+
+# sentence = " Milad said : 'i am programmer'"
+
+# sentence = 'Milad said : \' i am programmer \''
+
+sentence = " this is test text and \\ new line "
+print(sentence)
+
+
+print('---------------')
+
+# string Concatenation
+print(myName + " " + myFamily)  # Milad Parsa
+
+# result = myName + myAge
+
+# print(result)
+
+# print("my age is: " + myAge)
+
+print('---------------')
+
+courseName = "Python"
+
+courseName += " Course"
+
+# courseName = courseName + " Course"
+
+print(courseName)
