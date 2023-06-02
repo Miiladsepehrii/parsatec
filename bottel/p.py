14258bac35a0d58b3e196281a988c35f756e2ef1
Milad Parsa <82820198+miladparsa@users.noreply.github.com>
2023-06-02 10:30:58 +0330

    Delete bottel directory

--- bottel/p.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-
-api_id = '26616577'
-api_hash = 'b84c63aa98e2c8ee0ee09494f694382b'
-phone_number = '+989389232279'
-message = 'Hello, this is a test message.'
-
-with TelegramClient('session_name', api_id, api_hash) as client:
-    client.send_message(phone_number, message)
